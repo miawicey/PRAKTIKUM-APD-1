8a628624fb3bb8f85cd48a5f8a7024bbbcde1980
miawicey <vitakhairana@gmail.com>
2025-09-23 09:41:12 +0800

    ini update

--- praktikum-apd-1/Post Test/2509106041_LuvitaKhairanaSalwa_PT2.py
+++ praktikum-apd-1/Post Test/2509106041_LuvitaKhairanaSalwa_PT2.py
@@ -1,6 +1,7 @@
-print("Supermarket Kasir Vita")
+print("===== Supermarket Kasir Vita =====")
 print("brum brum brum")
-print("welcome to my supermarket")
+print("Welcome to my supermarket")
+print()
 
 # JENIS BARANG
 barang1 = "Susu"
@@ -13,39 +14,33 @@ harga2 = 3500
 harga3 = 4000
 
 # JUMLAH BARANG
-jumlah1 = 5
-jumlah2 = 6
+jumlah1 = 30
+jumlah2 = 20
 jumlah3 = 10
 
 # DISKON
-diskon = 0.5  # 50%
-ada_diskon = True
+diskon = 0.5   # 50%
+#  APAKAH ADA DISKON?
+ada_diskon = True   # kalau False, diskon otomatis 0
 
-# HITUNG TOTAL PER BARANG
+# PERTAMA KALI BELANJA LALU DIHITUNG
 total1 = harga1 * jumlah1
 total2 = harga2 * jumlah2
 total3 = harga3 * jumlah3
-
-# SUBTOTAL
 subtotal = total1 + total2 + total3
-
-# POTONGAN DISKON
-if ada_diskon:
-    potongan = subtotal * diskon
-else:
-    potongan = 0
-
-# TOTAL BAYAR
+potongan = subtotal * diskon * ada_diskon
 total_bayar = subtotal - potongan
 
 # CETAK STRUK
 print("Barang:", barang1, "| Harga:", harga1, "| Jumlah:", jumlah1, "| Total:", total1)
 print("Barang:", barang2, "| Harga:", harga2, "| Jumlah:", jumlah2, "| Total:", total2)
 print("Barang:", barang3, "| Harga:", harga3, "| Jumlah:", jumlah3, "| Total:", total3)
+print("-----------------------------------")
 print("Subtotal:", subtotal)
 print("Diskon 50%, potongan:", potongan)
 print("Total Bayar:", total_bayar)
 print("Ada Diskon?", ada_diskon)
+print()
 print("Terima Kasih")
-print("Datang lagi yaaa ke supermarket vita")
+print("Datang lagi yaaa ke supermarket Vita")
 print("brum brum brum")
